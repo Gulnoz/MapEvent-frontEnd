6e7adffadc5957b5d30534fb91f99920d20a8db6
Gulnoza Muminova-Boltaeva <33871558+Gulnoz@users.noreply.github.com>
2021-06-29 15:08:39 -0400

    Update LoginForm.js

--- src/Containers/LoginForm.js
+++ src/Containers/LoginForm.js
@@ -9,11 +9,7 @@ state = {
     errorMessege: null
 }
 
-// validateForm() {
-//     return this.state.email.length > 0 && this.state.password.length > 0;
-// }
-
-handleChange = event => {
+    handleChange = event => {
     this.setState({
         [event.target.name]: event.target.value
     });
@@ -26,8 +22,7 @@ errorMessegeHendler(message){
 }
 handleSubmit = event => {
     event.preventDefault();
-    fetch('https://mapevent-api.herokuapp.com/login', 
-        { 
+    fetch('https://mapevent-api.herokuapp.com/login', { 
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -37,13 +32,8 @@ handleSubmit = event => {
         }) 
         .then(res => res.json())
         .then((user) => {
-            //   return user ? this.props.logedIn() : null
-            console.log(user)
-            
             if (user.error === 'Not exist') {
-            
-                fetch('https://mapevent-api.herokuapp.com/users',
-                    {
+                fetch('https://mapevent-api.herokuapp.com/users', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({
@@ -53,54 +43,38 @@ handleSubmit = event => {
                     })
                     .then(res => res.json())
                     .then((user) => {
-                        
                         this.props.setCurrentUser(user.user.data);
                         localStorage.setItem('currentUserToken', user.jwt);
                     })
-                    
-
             }
             else if (user.error === 'Wrong password'){
                 this.errorMessegeHendler(user.error)
-                console.log(user.error)
             }
             else if (user.user) {
-                console.log(user)
                 this.props.setCurrentUser(user.user.data)
-
                 localStorage.setItem('currentUserToken', user.jwt);
             }
             else {
-               console.log(user)
-                
-                
+ 
             }
         })
 }
 
-
     render(){
-    return(
-    <Form onSubmit={this.handleSubmit} >  
-        <GoogleForm setCurrentUser={this.props.setCurrentUser}/>
-            <div style={{ color: 'white', padding: "10px", fontSize: "calc(10px + 2vmin)", fontWeight: "bold"}}>SignIn/SignUp form:</div>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label></Form.Label>
-            <Form.Control type="email" name="email" value={this.state.email} placeholder="Enter email" onChange={this.handleChange} />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-           
-            <Form.Control type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleChange} />
-                
-        </Form.Group>
-        {this.state.errorMessege
-           ? <div><Form.Label style={{color:'red'}}>{this.state.errorMessege}!!!</Form.Label></div>
-           : null}
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-            
-    </Form>
-    )
-    }
+        return(
+            <Form onSubmit={this.handleSubmit}>  
+                <GoogleForm setCurrentUser={this.props.setCurrentUser}/>
+                <div style={{ color: 'white', padding: "10px", fontSize: "calc(10px + 2vmin)", fontWeight: "bold"}}>SignIn/SignUp form:</div>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" name="email" value={this.state.email} placeholder="Enter email" onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Control type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleChange} />
+                </Form.Group>
+                {this.state.errorMessege ? <div><Form.Label style={{color:'red'}}>{this.state.errorMessege}!!!</Form.Label></div> : null}
+                <Button variant="primary" type="submit">
+                   Submit
+                </Button>
+            </Form>
+    )}
 }
