388c6261127af1d818aee395b2badfb818c62ee2
Gulnoza Muminova-Boltaeva <33871558+Gulnoz@users.noreply.github.com>
2021-06-29 14:55:51 -0400

    Update FavoritEvent.js

--- src/Containers/FavoritEvent.js
+++ src/Containers/FavoritEvent.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-
 export default class FavoritEvent extends React.Component {
-
-
-
     render() {
-        //console.log(this.props.favorit)
         return (
-            <li onClick={() => { this.props.popUpFavoriteHendler(this.props.favorit)}}><strong>
+            <li onClick={() => { this.props.popUpFavoriteHendler(this.props.favorit)}}>
+            <strong>
                 {this.props.favorit.name}
             </strong>
             </li>
