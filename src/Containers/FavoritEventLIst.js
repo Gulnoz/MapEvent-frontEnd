f6e7eb37d3e72ddd8434317e13e854952a013fd3
Gulnoza Muminova-Boltaeva <33871558+Gulnoz@users.noreply.github.com>
2021-06-29 15:01:04 -0400

    Update FavoritEventLIst.js

--- src/Containers/FavoritEventLIst.js
+++ src/Containers/FavoritEventLIst.js
@@ -5,28 +5,16 @@ import { Button } from "semantic-ui-react"
 export default class FavoritEventList extends React.Component {
 
     eventHendler = () => {
-        console.log("FavoritList")
-        console.log(this.props.favorits)
-        // console.log(this.props.favorits[0].name)
         return this.props.favorits.map(favorit => <FavoritEvent key={favorit.id} favorit={favorit} popUpFavoriteHendler={this.props.popUpFavoriteHendler}/>)
     }
 
     render() {
         return (
-            <>
-            <div>
-               
+            <div>  
             <h2 style={{color:"white"}}>{this.props.caption}</h2>
                 <ol class="favorit-content-div">{this.eventHendler()}</ol> 
-                    {/* <Button type='submit' onClick={this.props.createEventHendler}>Create Event</Button> */}
-
             </div>
-            
-           
-        </>
-        )
-    }
-
+     )}
 }
 
 
